4f400a935b406e98fd73c2f1e189acbe8a7c1662
Pablo Melendez <140300081@ucaribe.edu.mx>
2019-03-11 13:22:50 -0500

    agrega router al proyecto

--- front/src/App.js
+++ front/src/App.js
@@ -1,28 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux';
+import { Route, Switch, Redirect } from 'react-router';
+import Home from './containers/Home';
 
 class App extends Component {
   render() {
+    const { location } = this.props;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Switch location={location}>
+          <Route exact path="/" component={Home} />
+          <Redirect to="/" />
+        </Switch>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = ({ router }) => ({ location: router.location });
+
+export default connect(mapStateToProps)(App);
+
