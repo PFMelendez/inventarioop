4dc0f1557530cb1810c45d773b30cfc189e207ca
Raul Campos <Raul Campos>
2019-03-20 15:13:43 -0500

    Se arreglan estilos

--- front/src/App.js
+++ front/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Route, Switch, Redirect } from 'react-router';
 import Home from './containers/Home';
+import Navigation from './components/navegation';
 
 class App extends Component {
   render() {
     const { location } = this.props;
     return (
       <div>
+        <Navigation/>
         <Switch location={location}>
           <Route exact path="/" component={Home} />
           <Redirect to="/" />
@@ -20,4 +22,3 @@ class App extends Component {
 const mapStateToProps = ({ router }) => ({ location: router.location });
 
 export default connect(mapStateToProps)(App);
-
--- front/src/containers/Home/index.js
+++ front/src/containers/Home/index.js
@@ -1,25 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './styles.css'
+import FormApp from '../../components/formapp';
+import 'bootstrap/dist/css/bootstrap.css';
+import './styles.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <FormApp/>
       </div>
     );
   }
