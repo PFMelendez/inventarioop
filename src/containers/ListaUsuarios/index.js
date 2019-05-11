900488703c4f51060754518f9afb5861cf54b0b7
Raul Campos <Raul Campos>
2019-05-10 19:57:59 -0500

    se agrego vista de usuario detallado

--- src/containers/ListaUsuarios/index.js
+++ src/containers/ListaUsuarios/index.js
@@ -7,22 +7,17 @@ class ListaUsuarios extends Component {
         <div className="row mt-3">
           <div className="col-lg-12">
             <div className="card">
-              <div className="card-header"> Detalle del Usurio </div>
+              <div className="card-header">Usuarios</div>
               <div className="container" style={{ marginTop: 50, textAlign: 'center' }}> 
               <h4>Nombre</h4>
-              <hr />
-              <img src='https://screenshotlayer.com/images/assets/placeholder.png'></img>
-              <hr />
               <table className="table table-hover">
                 <thead className="thead-dark">
-                  <tr><th>Característica</th><th>Descripción</th></tr>
+                  <tr><th>Nombre usuario</th><th>Tipo usuario</th></tr>
                 </thead>
                 <tbody>
-                  <tr><th>Nombre</th><th>Raul Campos Adame</th></tr>
-                  <tr><th>Usuario</th><th>Admin@ucaribe.edu.mx</th></tr>
-                  <tr><th>Contraseña</th><th>HolaMundo</th></tr>
-                  <tr><th>Tipo usuario</th><th>Administrador</th></tr>
-                  <tr><th>PuestoDepartamental</th><th>EncargadoArea</th></tr>
+                <tr><th>RaulCampos</th><th>Administrador</th></tr>
+                <tr><th>Pablo melendes</th><th>Administrador</th></tr>
+                <tr><th>Raul Andrade</th><th>Capturista</th></tr>
                 </tbody>
               </table>
               </div>
