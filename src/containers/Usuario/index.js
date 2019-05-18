3c671373b86efb03d98ae56e2f497541025fe2ea
Raul Campos <Raul Campos>
2019-05-18 14:05:21 -0500

    se agrego funcionalidad a la vista crear usuarios

--- src/containers/Usuario/index.js
+++ src/containers/Usuario/index.js
@@ -1,7 +1,93 @@
 import React, { Component } from 'react';
+import api from '../../services/api';
 
 class Usuario extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      correo: '',
+      contrasena: '',
+      confirmarContrasena: '',
+      nombre: '',
+      apellidos: '',
+      nombreUsuario: '',
+      tipoUsuario: 0,
+      tiposUsuarios: [],
+    };
+    this.handleInputs = this.handleInputs.bind(this);
+    this.crearUsuario = this.crearUsuario.bind(this);
+  }
+
+  componentDidMount() {
+    const that = this;
+    api.tiposUsuarios.list()
+      .then(({ data }) => {
+        const { tiposUsuarios } = data;
+        that.setState({ tiposUsuarios });
+      })
+      .catch(err => {
+        alert('Hubo un error al cargar los tipos de usuarios');
+      })
+  }
+
+  handleInputs(e) {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  }
+
+  crearUsuario() {
+    const {
+      correo,
+      contrasena,
+      confirmarContrasena,
+      nombre,
+      apellidos,
+      nombreUsuario,
+      tipoUsuario,
+    } = this.state;
+    if (!correo || !contrasena || !confirmarContrasena || !nombre || !apellidos || !nombreUsuario || !tipoUsuario) {
+      alert('Debe llenar todos los cmapos');
+    } else {
+      const that = this;
+      api.usuarios.create({
+        correo,
+        contrasena,
+        nombre,
+        apellidos,
+        nombreUsuario,
+        tipoUsuario,
+      })
+        .then(response => {
+          alert(`Usuario creado correctamente. ID: ${response.data.usuario.id}`);
+          that.setState({
+            correo: '',
+            contrasena: '',
+            confirmarContrasena: '',
+            nombre: '',
+            apellidos: '',
+            nombreUsuario: '',
+            tipoUsuario: 0,
+          });
+        })
+        .catch(err => {
+          console.log(err);
+          alert('Ocurrio un problema creando el usuario');
+        })
+    }
+  }
+
   render() {
+    const {
+      correo,
+      contrasena,
+      confirmarContrasena,
+      nombre,
+      apellidos,
+      nombreUsuario,
+      tipoUsuario,
+      tiposUsuarios,
+    } = this.state;
+
     return (
       <div className="container-fluid">
         <div className="row mt-3">
@@ -9,10 +95,10 @@ class Usuario extends Component {
             <div className="card">
               <div className="card-header"> Detalle del Usurio </div>
               <div className="container" style={{ marginTop: 50, textAlign: 'center' }}>
-                <h4>Nombre</h4>
+                {/* <h4>Nombre</h4>
                 <hr />
                 <img src='https://screenshotlayer.com/images/assets/placeholder.png'></img>
-                <hr />
+                <hr /> */}
                 <table className="table table-hover">
                   <thead className="thead-dark">
                     <tr><th>Característica</th><th>Descripción</th></tr>
@@ -21,17 +107,33 @@ class Usuario extends Component {
                     <tr>
                       <td>
                         Nombre
-                    </td>
+                      </td>
                       <td>
-                        <input type="text" className="form-control" />
+                        <input type="text" className="form-control" name="nombre" value={nombre} onChange={this.handleInputs} />
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        Apellidos
+                      </td>
+                      <td>
+                        <input type="text" className="form-control" name="apellidos" value={apellidos} onChange={this.handleInputs} />
                       </td>
                     </tr>
                     <tr>
                       <td>
                         Usuario
-                    </td>
+                      </td>
                       <td>
-                        <input type="text" className="form-control" />
+                        <input type="text" className="form-control" name="nombreUsuario" value={nombreUsuario} onChange={this.handleInputs} />
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        Correo
+                      </td>
+                      <td>
+                        <input type="text" className="form-control" name="correo" value={correo} onChange={this.handleInputs} />
                       </td>
                     </tr>
                     <tr>
@@ -39,10 +141,9 @@ class Usuario extends Component {
                         Tipo usuario
                     </td>
                       <td>
-                        <select className='custom-select'>
+                        <select className='custom-select' name="tipoUsuario" value={tipoUsuario} onChange={this.handleInputs} >
                           <option value="">Seleccione...</option>
-                          <option value="">Administrador</option>
-                          <option value="">Capturista</option>
+                          {tiposUsuarios.map(item => <option value={item.id}>{item.nombre}</option>)}
                         </select>
                       </td>
                     </tr>
@@ -51,10 +152,18 @@ class Usuario extends Component {
                         Contraseña
                     </td>
                       <td>
-                        <input type="password" className="form-control" />
+                        <input type="password" className="form-control" name="contrasena" value={contrasena} onChange={this.handleInputs} />
                       </td>
                     </tr>
                     <tr>
+                      <td>
+                        Confirmar Contraseña
+                    </td>
+                      <td>
+                        <input type="password" className="form-control" name="confirmarContrasena" value={confirmarContrasena} onChange={this.handleInputs} />
+                      </td>
+                    </tr>
+                    {/* <tr>
                       <td>
                         Foto
                       </td>
@@ -63,12 +172,16 @@ class Usuario extends Component {
                           <div className="custom-file">
                             <input type="file" className="custom-file-input" />
                             <label className="custom-file-label" />
-                          </div>
-                          {/* <div className="input-group-append">
+                          </div> 
+                        <div className="input-group-append">
                             <span className="input-group-text">Examinar</span>
-                          </div> */}
+                          </div> *
                         </div>
                       </td>
+                    </tr> */}
+                    <tr>
+                      <td><button className="btn btn-primary" type="button" onClick={this.crearUsuario}>Crear usuario</button></td>
+                      <td><hr /></td>
                     </tr>
                   </tbody>
                 </table>
@@ -76,7 +189,7 @@ class Usuario extends Component {
             </div>
           </div>
         </div>
-      </div>
+      </div >
     )
   }
 }
--- src/services/api.js
+++ src/services/api.js
@@ -26,5 +26,8 @@ export default {
   },
   estados: {
     list: () => axios.get('/estado'),
+  },
+  tiposUsuarios: {
+    list: () => axios.get('/tiposUsuarios')
   }
 }
